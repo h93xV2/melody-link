f8052d407d5378f3fd9813840f72b2e79f461afe
h93xV2 <h93xV2@users.noreply.github.com>
2024-05-17 02:19:14 -0700

    Add file uploads

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,7 +3,7 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'audioFiles',
   access: (allow) => ({
-    'audio/*': [
+    'audio/{entity_id}/*': [
       allow.guest.to(["read"]),
       allow.entity('identity').to(["read","write","delete"])
     ]
--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,43 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
+import { useState } from "react";
+
+import { StorageManager } from "@aws-amplify/ui-react-storage";
+
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import { Authenticator } from '@aws-amplify/ui-react'
 import "@aws-amplify/ui-react/styles.css";
-import { uploadData } from 'aws-amplify/storage';
+import { fetchUserAttributes } from "aws-amplify/auth";
 
 Amplify.configure(outputs);
 
-const client = generateClient<Schema>();
-
 export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
-
-  useEffect(() => {
-    listTodos();
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
-  }
-
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({id});
-  }
+  const [displayName, setDisplayName] = useState("");
 
   return (
-    <Authenticator signUpAttributes={['email', 'preferred_username']}>
-      {({ signOut, user }) => (
-        <main>
-          <h1>{user?.username}</h1>
-          <button onClick={signOut}>Sign Out</button>
-        </main>
-      )}
+    <Authenticator signUpAttributes={['email', 'preferred_username']} >
+      {(signOut) => {
+        fetchUserAttributes().then((attributes) => {
+          const preferredUsername = attributes.preferred_username;
+          if (preferredUsername) {
+            setDisplayName(preferredUsername);
+          }
+        });
+
+        return (
+          <main>
+            <h1>Hi {displayName}</h1>
+            <StorageManager
+              acceptedFileTypes={['audio/*']}
+              path="audio/"
+              maxFileCount={1}
+              isResumable
+            />
+          </main>
+        )
+      }}
     </Authenticator>
   );
 }
