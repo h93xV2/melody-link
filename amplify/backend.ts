03604c497f8c29e0713878100b70cf91ce863d23
h93xV2 <h93xV2@users.noreply.github.com>
2024-05-25 16:49:51 -0700

    Update lambda permissions

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,18 +3,20 @@ import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
 import { storage } from './storage/resource';
 import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { preSignUp } from './auth/pre-sign-up/resource.js';
 
 const backend = defineBackend({
   auth,
   data,
-  storage
+  storage,
+  preSignUp
 });
 
-backend.auth.resources.unauthenticatedUserIamRole.addToPrincipalPolicy(
-  new PolicyStatement({
-    actions: [
-      "cognito-idp:ListUsers"
-    ],
-    resources: ["*"]
-  })
-);
+const statement = new PolicyStatement({
+  actions: [
+    "cognito-idp:ListUsers"
+  ],
+  resources: [backend.auth.resources.userPool.userPoolArn]
+})
+
+backend.preSignUp.resources.lambda.addToRolePolicy(statement);
