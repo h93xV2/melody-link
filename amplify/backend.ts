703a573e34f96adbe2b083d1a60d1671eadd7f83
h93xV2 <h93xV2@users.noreply.github.com>
2024-05-25 16:07:52 -0700

    Update IAM role

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,9 +2,19 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
 import { storage } from './storage/resource';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
   storage
 });
+
+backend.auth.resources.unauthenticatedUserIamRole.addToPrincipalPolicy(
+  new PolicyStatement({
+    actions: [
+      "cognito-idp:ListUsers"
+    ],
+    resources: ["*"]
+  })
+);
